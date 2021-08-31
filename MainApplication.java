ced98a2cb9c7ee9b66f375b197d35153bc3d998b
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2021-09-01 01:25:34 +0900

    Add files via upload

---
+++ MainApplication.java
@@ -0,0 +1,31 @@
+import dao.StudentDao;
+import dto.StudentDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    /*
+        try{
+            StudentDao studentDao = new StudentDao();
+            List<StudentDto> students = new ArrayList<StudentDto>();
+
+            students = studentDao.takeOut();
+
+            for(StudentDto stud : students){
+                System.out.println(stud);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+     */
+
+}
